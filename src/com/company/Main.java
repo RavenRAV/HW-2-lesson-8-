23e2a6a56e440b414093efe6c8ab8bf856613aa7
RavenRav <akilaia@inbox.ru>
2019-08-15 19:35:40 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Box<Double, Integer> box1 = new Box<>(2.1, 2);
+        Box<Float, Float> box2 = new Box<>(3.14f, 6.1f);
+
+        addition(box1, box2);
+        multiplication(box1, box2);
+
+
+    }
+
+    public static void addition(Box<? extends Double, Integer> box1, Box<? extends Float, Float> box2) {
+
+        Number ad = box1.getNumber1() + box1.getNumber2() + box2.getNumber2() + box2.getNumber1();
+        System.out.println(ad);
+
+    }
+
+    public static void multiplication(Box<? extends Double, Integer> box1, Box<? extends Float, Float> box2) {
+        Number mu = box1.getNumber1() * box1.getNumber2() * box2.getNumber2() * box2.getNumber1();
+        System.out.println(mu);
+
+
+    }
+}
